344f3fdda9c4aa3ea7b0952a799129b8f4c5e9f5
frenzyali <159234319+frenzyali@users.noreply.github.com>
2024-02-19 16:09:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Question: 1
+// console.log("Hello World");
+// Question: 2
+// let message = "Ali";
+// console.log("Hey " + message + ", Would you like to learn some programming?");
---
+++ main.ts
@@ -0,0 +1,6 @@
+// Question: 1
+// console.log("Hello World");
+
+// Question: 2
+// let message = "Ali";
+// console.log("Hey " + message + ", Would you like to learn some programming?");
