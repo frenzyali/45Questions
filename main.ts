ada51574006e372b912e780bad5dbeed3cf572fd
frenzyali <syedpheonixhussain@gmail.com>
2024-02-21 13:46:29 +0500

    I completed question:3

--- main.ts
+++ main.ts
@@ -4,3 +4,9 @@
 // Question: 2
 // let message = "Ali";
 // console.log("Hey " + message + ", Would you like to learn some programming?");
+
+// Question: 3
+// let Name = "Ali Hussain";
+// console.log(Name.toLowerCase());
+// console.log(Name.toUpperCase());
+// console.log(Name[0].toUpperCase()+Name[1]+Name[2] + " " + Name[4].toUpperCase()+Name[5]+Name[6]+Name[7]+Name[8]+Name[9]+Name[10]);
