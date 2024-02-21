2492e62371ce48522bf43e7d7e468f7a4a7499d5
frenzyali <syedpheonixhussain@gmail.com>
2024-02-21 14:25:20 +0500

    Completed Till Question: 10

--- main.ts
+++ main.ts
@@ -1,12 +1,63 @@
-// Question:1
-// console.log("Hello World");
-
-// Question:2
+// Question: 2
 // let message = "Ali";
 // console.log("Hey " + message + ", Would you like to learn some programming?");
 
-// Question:3
+
+
+// Question: 3
 // let Name = "Ali Hussain";
 // console.log(Name.toLowerCase());
 // console.log(Name.toUpperCase());
 // console.log(Name[0].toUpperCase()+Name[1]+Name[2] + " " + Name[4].toUpperCase()+Name[5]+Name[6]+Name[7]+Name[8]+Name[9]+Name[10]);
+
+
+
+// Question: 4
+// console.log('Kiyomi Takada once said, "The only true fear is the fear of the unknown."');
+
+
+
+// Question: 5
+// let AuthorName = "Sun Tzu";
+// let Quote = "In order to know your enemy, you must become your enemy";
+// console.log(AuthorName + ' once said,' + ' "' + Quote + '."');
+
+
+
+// Question: 6
+// let Name = 'Ali Hussain';
+// console.log("\t"+Name+"\t\n"+Name);
+
+
+
+// Question: 7
+// console.log(2 + 6);
+// console.log(11 - 3);
+// console.log(4 * 2);
+// console.log(16 / 2);
+
+
+
+// Question: 9
+// let FavoriteNumber = 27;
+// console.log("This is my favorite number: " + FavoriteNumber);
+
+
+
+// Question: 10
+// Comment for Program 3: This program takes the value in the variable and converts it to lowercase, uppercase and titlecase by using the following code
+// let Name = "Ali Hussain";
+// console.log(Name.toLowerCase());
+// console.log(Name.toUpperCase());
+// console.log(Name[0].toUpperCase()+Name[1]+Name[2] + " " + Name[4].toUpperCase()+Name[5]+Name[6]+Name[7]+Name[8]+Name[9]+Name[10]);
+// Comment for Program 5: This program takes two values and prints them with the name of the Author and their quote with proper punctuation.
+// let AuthorName = "Sun Tzu";
+// let Quote = "In order to know your enemy, you must become your enemy";
+// console.log(AuthorName + ' once said,' + ' "' + Quote + '."');
+
+
+
+
+
+
+
