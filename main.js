cc608295d87f8104f34bc8653b235602c4aa89e7
frenzyali <syedpheonixhussain@gmail.com>
2024-02-24 14:02:23 +0500

    Commiting till question:18

--- main.js
+++ main.js
@@ -33,3 +33,82 @@
 // let AuthorName = "Sun Tzu";
 // let Quote = "In order to know your enemy, you must become your enemy";
 // console.log(AuthorName + ' once said,' + ' "' + Quote + '."');
+// Question: 11
+// let names = ["Ali", "John", "Ayan", "Ammar"];
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+// Question: 12
+// let names = ["Ali", "John", "Ayan", "Ammar"];
+// for (let n of names){
+//     console.log(`Hello ${n}, have a great day!`);
+// }
+// Question: 13
+// let items = ["Honda Civic", "Ninja H2R", "Audi R6", "Mercedes S Class"];
+// for (let n of items){
+//     if (n === "Honda Civic"){
+//         console.log("The engine of Honda Civic is very modern.");
+//     } else if (n === "Ninja H2R"){
+//         console.log("Ninja H2R is one of the fastest motorcycles in the world.");
+//     } else if (n === "Audi R6"){
+//         console.log("Audi R6 is one of the most famous cars of Audi.");
+//     } else if (n === "Mercedes S Class"){
+//         console.log("The interior of Mercedes S Class is super comfortable");
+//     }
+// }
+// Question: 14
+// let guestList = ["Luffy", "Naruto", "Goku"];
+// for (let guest of guestList){
+//      console.log(`Hello ${guest}, please be sure to attend the dinner on 24th of February at 8:00PM sharp.`);
+//  }
+// Qustion: 15
+// let guestList = ["Luffy", "Naruto", "Goku"];
+//  for (let guest of guestList){
+//       console.log(`Hello ${guest}, please be sure to attend the dinner on 24th of February at 8:00PM sharp.`);
+//   }
+// console.log("\n\nUnfortunately, Naruto will not be able to attend the dinner party.\n\n");
+// guestList[1] = "Eren";
+// for (let guest of guestList){
+//     console.log(`Hey ${guest}, I would be delighted if you attend the grand dinner party at my house on 25th of February at 9:00PM.`);
+// }
+// Question: 16
+// let guestList = ["Luffy", "Eren", "Goku"];
+//  for (let guest of guestList){
+//       console.log(`Hey ${guest}, I would be delighted if you attend the grand dinner party at my house on 25th of February at 9:00PM.`);
+//   }
+// console.log("\nHey everyone, I found a bigger dining table so I will be inviting more people.\n");
+// guestList.unshift("Kaneki");
+// let index = Math.floor(guestList.length / 2);
+// guestList.splice(index, 0, "Ichigo");
+// guestList.push("Natsu");
+// for (let guest of guestList){
+//     console.log(`Salam ${guest}, Please be sure to attend the dinner party at my home tomorrow.`);
+// }
+// Question: 17
+var guestList = ["Luffy", "Eren", "Goku"];
+for (var _i = 0, guestList_1 = guestList; _i < guestList_1.length; _i++) {
+    var guest = guestList_1[_i];
+    console.log("Hey ".concat(guest, ", I would be delighted if you attend the grand dinner party at my house on 25th of February at 9:00PM."));
+}
+console.log("\nHey everyone, I found a bigger dining table so I will be inviting more people.\n");
+guestList.unshift("Kaneki");
+var index = Math.floor(guestList.length / 2);
+guestList.splice(index, 0, "Ichigo");
+guestList.push("Natsu");
+for (var _a = 0, guestList_2 = guestList; _a < guestList_2.length; _a++) {
+    var guest = guestList_2[_a];
+    console.log("Salam ".concat(guest, ", Please be sure to attend the dinner party at my home tomorrow."));
+}
+console.log("\nI am really sorry and embarassed to say that the dining table broke and I can only invite two people now.\n");
+while (guestList.length > 2) {
+    var removedGuest = guestList.pop();
+    console.log("I am sorry that I won't be able to invite ".concat(removedGuest, " to the dinner."));
+}
+for (var _b = 0, guestList_3 = guestList; _b < guestList_3.length; _b++) {
+    var n = guestList_3[_b];
+    console.log("".concat(n, ", You are still invited!"));
+}
+while (guestList.length > 0) {
+    guestList.pop();
+}
+console.log(guestList);
